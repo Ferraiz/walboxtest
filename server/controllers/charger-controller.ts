501acc09bf7f61093efe0bae39df9dd4dd02552f
adrian.steller.contractor@bbva.com <adrian.steller.contractor@bbva.com>
2021-03-13 18:39:33 +0100

    The controller retrives the deviceId

--- server/controllers/charger-controller.ts
+++ server/controllers/charger-controller.ts
@@ -1,14 +1,18 @@
 import { chargerIncommingFacade, wsIncommingFacade} from '../facades';
+import { chargerRepository } from '../repositories';
 
 class ChargerController {
 
-  public wsIncommingMessage(message: string, url: string) {
+  public async wsIncommingMessage(message: string, url: string) {
     try {
       const incommingCharger: WsIncommingCharger = wsIncommingFacade.validateSchema(message);
       const idCharger:string = chargerIncommingFacade.validateIdUrl(url);
 
       console.log('NNN incommingCharger: ', incommingCharger);
       console.log('NNN idCharger: ', idCharger);
+
+      const deviceId = await chargerRepository.getDeviceId(idCharger);
+      console.log('NNN deviceId: ', deviceId);
     } catch(err) {
       console.error(err.customMessage);
     }
